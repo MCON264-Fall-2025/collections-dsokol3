ec48bf5d00e90aa7172556269e72c7d721ee7581
dsokol3 <dsokol3@student.touro.edu>
2025-11-27 21:10:16 -0500

    bag

--- src/main/java/exercises/BasicBag.java
+++ src/main/java/exercises/BasicBag.java
@@ -4,21 +4,42 @@ import edu.touro.mcon264.apps.collections.ArrayCollection;
 import edu.touro.mcon264.apps.collections.BagInterface;
 
 public class BasicBag<T> extends ArrayCollection<T> implements BagInterface<T> {
+
+
+    /**
+     * If this bag is not empty, removes and returns a random element of the bag;
+     * otherwise returns null.
+     */
     @Override
     public T grab() {
         return null;
     }
 
+    /**
+     * Returns a count of all elements e in this collection such that e.equals(target).
+     *
+     * @param target
+     */
     @Override
     public int count(T target) {
         return 0;
     }
 
+    /**
+     * Removes all elements e from this collection such that e.equals(target)
+     * and returns the number of elements removed.
+     *
+     * @param target
+     * @return
+     */
     @Override
     public int removeAll(T target) {
         return 0;
     }
 
+    /**
+     * Empties this bag so that it contains zero elements.
+     */
     @Override
     public void clear() {
 
